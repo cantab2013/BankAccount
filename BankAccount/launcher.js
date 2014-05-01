8c60fa595b6318d60b30273e619322a654fd1ba7
matt <mtbrooks33@gmail.com>
2014-05-01 19:43:49 -0400

    work on loading screen / launcher

--- BankAccount/launcher.js
+++ BankAccount/launcher.js
@@ -3,28 +3,29 @@
  */
 
 $(function() {
-	
+
+	loading();
+
+	$.getScript('main.js', function() {
+		open();
+	}, 2000);
+
 	function loading() {
 
-		/** presents loading screen * */
-		$('body').css('display','none');
-		$('html').append('<div id="loading"><p>Loading..</p></div>');
+		/** presents loading animation * */
+		$('body').css('display', 'none');
+		$('html').append('<div id="loading">Loading... Please Wait.</div>');
 
-	};
+	}
+	;
 
 	function open() {
 
 		/** presents application and removes loading screen * */
-
 		$('#loading').fadeOut(function() {
 			$('body').fadeIn(400);
 		});
-	};
-	
-	$.when(loading()).done(function() {
-		$.getScript('main.js', function() {
-			open();
-		});
-	});
-	
+	}
+	;
+
 });
